c97625251692b02520a32f86392a003f0133c796
MauroRodriguez23 <maurorodriguez40@hotmail.com>
2022-04-21 20:23:19 -0300

    main

--- JS/general.js
+++ JS/general.js
@@ -1,180 +1,118 @@
-const objCafeteria = {
-    cafe: "Café c/s leche doble $120 \n",
-  
-
-    matecocido: "Matecocido o té, $100 \n", 
-    
-
-    capuccino:  "Capuccino $140 \n", 
-    
-    medialunas: "Medialunas dulces/saladas $80 c/u \n", 
-    
-    
-    tostado:     "Tostado $300 \n",
-   
-
-    carlito: "Carlito común $350 \n",
- 
-
+if(!JSON.parse(localStorage.getItem("carrito"))) {
+    localStorage.setItem("carrito",JSON.stringify([]))
 }
+const carritoInicial = JSON.parse(localStorage.getItem("carrito"));
+const carrito = carritoInicial;
 
+    // ARRAY CAFETERIA
+const objCafeteria = [
+    {producto: "Café c/s leche doble",
+    precio: 120,
+    img: "/IMG/cafe.jpg",
+    id: "cafe"},
 
+    {producto: "Matecocido o té", 
+    precio: 100,
+    img: "/IMG/te.jpg",
+    id: "te"},
 
-const precioCafeteria = {
-    precioCafe: 120, 
-    precioMatecocido: 100,
-    precioCapuccino: 140,
-    precioMedialunas: 80,
-    precioTostado: 300,
-    precioCarlito: 350
+    {producto:  "Capuccino", 
+    precio: 140,
+    img: "/IMG/capuccino.jpeg",
+    id : "capuccino"},
+
+    {producto: "Medialunas dulces/saladas", 
+    precio: 80,
+    img: "/IMG/medialuna.jpg",
+    id: "medialuna"},
+    
+    {producto:     "Tostado",
+    precio: 300,
+    img: "/IMG/tostado.jpg",
+    id: "tostado"},
+
+    {producto: "Carlito común",
+    precio: 350,
+    img: "/IMG/carlito.jpg",
+    id: "carlito"}
+]
+
+for (let i = 0; i < objCafeteria.length; i++) {
+    const element = objCafeteria[i];
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.setAttribute("id", element.id)
+    div.innerHTML = `<span class="span_card" >${element.producto + " $" + element.precio}</span>
+                     <div class="img_card">
+                        <img src=${element.img} alt="fotoProducto" />
+                     </div>
+                     <button class="btn_comprar">comprar</button>`;
+    const section = document.getElementById("nuestra_carta"); 
+    section.appendChild(div);
 }
-
-// objCafeteria = JSON.parse;
-// precioCafeteria = JSON.parse;
-
-
-
-
-
-
-
-const totalCompro = [];
 const reducer = (accumulator, curr) => accumulator + curr;
 
-const totalPrecio = [];
 
+/* BOTON COMPRAR */
 
+let comprar = document.getElementsByClassName("btn_comprar");
 
+for (let i = 0; i < comprar.length; i++) {
+    const element = comprar[i];
+    element.addEventListener("click", clickCafe);
+}
 
-
-
-
-/* BOTON CAFE */
-
-let comprar_cafe = document.getElementById("btn_cafe");
-comprar_cafe.addEventListener("click", clickCafe);
-
-function clickCafe () {
+function clickCafe (e) {
     document.getElementById("agregado_carrito").style.display = 'block'; 
 
-
-    totalCompro.push(objCafeteria.cafe);
-    totalPrecio.push(precioCafeteria.precioCafe);
-
+    const evento = e.target
+    const nombreProducto = evento.parentNode.getAttribute("id");
     
-
-    let imprime_resultado = document.getElementById("carrito_menu");
-    imprime_resultado.innerText = totalCompro.reduce(reducer);
-
+    function finderId(item) {
+        return (item.id == nombreProducto) 
     }
 
-
-
-/* BOTON MATECOCIDO */
-
-let comprar_matecocido = document.getElementById("btn_matecocido");
-comprar_matecocido.addEventListener("click", clickMatecocido);
-
-function clickMatecocido() {
-    document.getElementById("agregado_carrito").style.display = 'block';
-
-    totalCompro.push(objCafeteria.matecocido)
-    totalPrecio.push(precioCafeteria.precioMatecocido);
-
-    let imprime_resultado = document.getElementById("carrito_menu");
-    imprime_resultado.innerText = totalCompro.reduce(reducer);  
-}
-
-
-/* BOTON CAPUCCINO */
-
-
-
-let comprar_capuccino = document.getElementById("btn_capuccino");
-comprar_capuccino.addEventListener("click", clickCapuccino);
-
-function clickCapuccino() {
-    document.getElementById("agregado_carrito").style.display = 'block';
-
+    const producto = objCafeteria.find(finderId);
+    carrito.push(producto);
+    localStorage.setItem("carrito",JSON.stringify(carrito));
    
-    totalCompro.push(objCafeteria.capuccino);
-    totalPrecio.push(precioCafeteria.precioCapuccino);
     let imprime_resultado = document.getElementById("carrito_menu");
-    imprime_resultado.innerText = totalCompro.reduce(reducer);
+    imprime_resultado.innerText =  nombreProducto;
+    
+    // imprime_resultado.innerText = localStorage.getItem("carrito",JSON.stringify(carrito.reduce(reducer)));
+
+     /*LIBRERIA PARA PONER ALERTS*/
+    // Swal.fire({
+    //     icon: 'success',
+    //     backdrop: 'rgb(24, 37, 44)',
+    //     imageUrl: '/IMG/cafe.jpg',
+    //     imageWidth: 400,
+    //     imageHeight: 360,
+    //     background: 'black',
+    //     title: '¡Nuevo producto al carrito!',
+    //     showConfirmButton: false,
+    //     color: 'green',
+    //     timer: 1500
+    // })    
 }
-
-
-
-
-
-/* BOTON MEDIALUNAS */
-
-
-let comprar_medialunas = document.getElementById("btn_medialunas");
-comprar_medialunas.addEventListener("click", comprarMedialunas);
-
-function comprarMedialunas() {
-    document.getElementById("agregado_carrito").style.display = 'block';
-
-    totalCompro.push(objCafeteria.medialunas);
-    totalPrecio.push(precioCafeteria.precioMedialunas);
-    let imprime_resultado = document.getElementById("carrito_menu");
-    imprime_resultado.innerText = totalCompro.reduce(reducer);
-}
-
-
-/* BOTON TOSTADO */
-
-
-let comprar_tostado = document.getElementById("btn_tostado");
-comprar_tostado.addEventListener("click", comprarTostado);
-
-function comprarTostado() {
-    document.getElementById("agregado_carrito").style.display = 'block';
-
-    totalCompro.push(objCafeteria.tostado);
-    totalPrecio.push(precioCafeteria.precioTostado);
-    let imprime_resultado = document.getElementById("carrito_menu");
-    imprime_resultado.innerText = totalCompro.reduce(reducer);
-}
-
-/* BOTON CARLITO */
-
-
-let comprar_carlito = document.getElementById("btn_carlito");
-comprar_carlito.addEventListener("click", comprarCarlito);
-
-function comprarCarlito() {
-    document.getElementById("agregado_carrito").style.display = 'block';
-
-    totalCompro.push(objCafeteria.carlito);
-    totalPrecio.push(precioCafeteria.precioCarlito);
-    let imprime_resultado = document.getElementById("carrito_menu");
-    imprime_resultado.innerText = totalCompro.reduce(reducer);
-}
-
-
-
 
 /* VER TOTAL */ 
 
 let ver_total = document.getElementById("ver_total");
 ver_total.addEventListener("click", verTotal);
 
-function verTotal() {
-    let imprime_resultado = document.getElementById("carrito_menu");
-    imprime_resultado.innerText = totalPrecio.reduce(reducer);
+function verTotal(e) {
 }
 
 
-/* SALIR */
+// /* SALIR */
 
-let sali = document.getElementById("salir");
-sali.addEventListener("click", salir);
+// let sali = document.getElementById("salir");
+// sali.addEventListener("click", salir);
 
 
-function salir() {
-    document.getElementById("carrito_menu").style.display = 'none';
-    document.getElementById("botones").style.display = 'none';
-}
+// function salir() {
+//     document.getElementById("carrito_menu").style.display = 'none';
+//     document.getElementById("botones").style.display = 'none';
+// }
 
