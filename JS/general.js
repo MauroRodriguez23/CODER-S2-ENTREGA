dc46638612f33c8f2c9466bd7791062e85c84531
MauroRodriguez23 <maurorodriguez40@hotmail.com>
2022-05-14 03:48:15 -0300

    main

--- JS/general.js
+++ JS/general.js
@@ -4,45 +4,59 @@ if(!JSON.parse(localStorage.getItem("carrito"))) {
 const carritoInicial = JSON.parse(localStorage.getItem("carrito"));
 const carrito = carritoInicial;
 
+if(!JSON.parse(localStorage.getItem("total"))) {
+    localStorage.setItem("total",JSON.stringify([]))
+}
+const totalIni = JSON.parse(localStorage.getItem("total"));
+const totalStore = totalIni;
+
+
 
     // ARRAY CAFETERIA
 const objCafeteria = [
     {producto: "Café c/s leche doble",
     precio: 120,
     img: "/IMG/cafe.jpg",
-    id: "cafe",
+    id: "Café c/s leche doble $120 \n",
+    idPrecio: 120,
     p: "El café con leche es una bebida de café común en España y América Latina <br> que consiste en café fuerte (generalmente espresso) mezclado con leche escaldada en cantidades aproximadamente iguales."},
 
     {producto: "Matecocido o té", 
     precio: 100,
     img: "/IMG/te.jpg",
-    id: "te",
+    id: "Matecocido o té $100 \n",
+    idPrecio: 100,
     p: "El té proviene principalmente de <br> China continental, <br> India, Sri Lanka, Taiwán, <br> Japón, Nepal, Australia, <br> Argentina y Kenia."},
 
     {producto:  "Capuccino", 
     precio: 140,
     img: "/IMG/capuccino.jpeg",
-    id : "capuccino",
+    id : "Capuccino $140 \n",
+    idPrecio: 140,
     p: "El capuchino (del italiano cappuccino) <br> es una bebida nacida en Italia, <br> preparada con café expreso y <br> leche montada con vapor <br> para darle cremosidad."},
 
     {producto: "Medialunas dulces/saladas", 
     precio: 80,
     img: "/IMG/medialuna.jpg",
-    id: "medialuna",
+    id: "Medialunas dulces/saladas $80 \n",
+    idPrecio: 80,
     p: "En Argentina, Chile, Paraguay y Uruguay, el término medialuna es un panificado dulce/salado elaborado para comer en el desayuno o la merienda, también llamado croissant."},
     
-    {producto:     "Tostado",
+    {producto: "Tostado",
     precio: 300,
     img: "/IMG/tostado.jpg",
-    id: "tostado",
+    id: "Tostado $300 \n",
+    idPrecio: 300,
     p: "Un tostado es un sándwich de miga <br> que se tuesta <br> hasta que sus tapas <br> de miga queden doradas y crocantes. <br> El más común es el de jamón y queso."},
 
     {producto: "Carlito común",
     precio: 350,
     img: "/IMG/carlito.jpg",
-    id: "carlito",
+    id: "Carlito común $350 \n",
+    idPrecio: 350,
     p: "Al igual que el tostado se tuesta de <br> ambos lados, la diferencia es que está preparado <br> con jamón, queso, pan de miga, manteca y ketchup."}
 ]
+
 
 for (let i = 0; i < objCafeteria.length; i++) {
     const element = objCafeteria[i];
@@ -63,7 +77,22 @@ for (let i = 0; i < objCafeteria.length; i++) {
     section.appendChild(p);
 }
 
+
+  
+      // ABRIR CARD
+
+
+ // click.style.height = '770';
+
+
+
+
 /* BOTON COMPRAR */
+const productoss = [];
+const total = [];
+const reducer = (accumulator, curr) => accumulator + curr;
+
+
 
 let comprar = document.getElementsByClassName("btn_comprar");
 
@@ -74,18 +103,41 @@ for (let i = 0; i < comprar.length; i++) {
 
 function clickCafe (e) {
     const evento = e.target
-    const nombreProducto = evento.parentNode.getAttribute("id");
+    const nombreProducto = evento.parentNode.getAttribute('id');
+    
 
     function finderId(item) {
         return (item.id == nombreProducto);
     }
 
     const producto = objCafeteria.find(finderId);
+    productoss.push(nombreProducto);
     carrito.push(producto);
     localStorage.setItem("carrito",JSON.stringify(carrito));
-   
+ //ACÁ TERMINA EL PRODUCTO
+ 
+    
+    // const evento2 = e.target
+    // const precios = evento2.parentNode.getAttribute('idPrecio');
+
+    // function finderPrecio() {
+    //     console.log("hola");
+    //     // return (item.idPrecio == precios);
+    // }
+
+    // const elPrecio = objCafeteria.find(finderPrecio);
+
+    const elPrecio = objCafeteria[1];
+    total.push(objCafeteria[1])
+    
+    // total.push(elPrecio);
+    totalStore.push(elPrecio);
+    localStorage.setItem("total",JSON.stringify(totalStore));
+  
+
+
     let imprime_resultado = document.getElementById("carrito_menu");
-    imprime_resultado.innerText = nombreProducto;
+    imprime_resultado.innerText = productoss.reduce(reducer);
    
 
      /*LIBRERIA PARA PONER ALERTS*/
@@ -105,12 +157,23 @@ function clickCafe (e) {
 
 /* VER TOTAL */ 
     let ver_total = document.getElementById("ver_total");
-    ver_total.addEventListener("click", verTotal);
+    ver_total.addEventListener("click", calcularTotal);
 
-    function verTotal() {
+    // function verTotal() {
         
-   
-}
+    // }
+      // * Calcula el precio total teniendo en cuenta los productos repetidos
+        // */
+       function calcularTotal() {
+          
+           let imprime_resultado = document.getElementById("carrito_menu");
+           imprime_resultado.innerText = total.reduce(reducer);
+       }
+
+
+
+
+
 
 
 
--- JS/slider.js
+++ JS/slider.js
@@ -1,52 +1,28 @@
-// const objCafeteria = {
-//     cafe: "Café c/s leche doble $120 \n",
-//     precioCafe: 120, 
-
-//     matecocido: "Matecocido o té, $100 \n", 
-//     precioMatecocido: 100,
-
-//     capuccino:  "Capuccino $140 \n", 
-//     precioCapuccino: 140 ,
-    
-//     medialunas: "Medialunas dulces/saladas $80 c/u \n", 
-//     precioMedialunas: 80,
-    
-//     tostado:     "Tostado $300 \n",
-//     precioTostado: 300,
-
-//     carlitoComun: "Carlito común $350 \n",
-//     precioCarlitoComun: 350,
-
-//     carlitoEspecial: "Carlito especial $450 \n",
-//     precioCarlitoEspecial: 450
-            
-// }
-
 
 window.addEventListener('load', cambiarSlider) 
 
  
     let imagenes_slider = [];
 
-    imagenes_slider[0] = '/IMG/slider1.jpg';
-    imagenes_slider[1] = '/IMG/slider2.jpg';
-    imagenes_slider[2] = '/IMG/slider3.jpg';
+    imagenes_slider[0] = '/IMG/Fondo_inicio.jpg';
+    // imagenes_slider[1] = '/IMG/slider2.jpg';
+    // imagenes_slider[2] = '/IMG/slider3.jpg';
    
     let contadorSlider = 0;
 
 
-function cambiarSlider() {
-    document.slider.src = imagenes_slider[contadorSlider];
+// function cambiarSlider() {
+//     document.slider.src = imagenes_slider[contadorSlider];
 
-    if (contadorSlider < 2) {
-        contadorSlider++;
-    } else {
-        contadorSlider = 0;
-    }
-}
-    setInterval(cambiarSlider, 2000);
+//     if (contadorSlider < 2) {
+//         contadorSlider++;
+//     } else {
+//         contadorSlider = 0;
+//     }
+// }
+//     setInterval(cambiarSlider, 2000);
 
-cambiarSlider();
+// cambiarSlider();
 
 
 
